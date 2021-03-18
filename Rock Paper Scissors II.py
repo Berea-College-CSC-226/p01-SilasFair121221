cbf132c8ce88c6aa50fd3809e8175c4da76fc55c
fairs <fairs@gmail.com>
2021-03-17 23:49:42 -0400

    Added score to scoreboard and improved visuals. Added a computer prediction based on the player's history of moves.

--- Rock Paper Scissors II.py
+++ Rock Paper Scissors II.py
@@ -18,10 +18,19 @@ class Wn:
     wn.bgcolor("lightgreen")  # Set the background color
     wn.bgpic("gifs/final_project_background.gif")
 
-class State: # the state machine object
-    choosing = True
-    player_won = False
-    computer_won = False
+
+
+
+class Tutorial:
+    # this text pops up on the screen and shows the user how to play
+    # when the player presses a button it disappears
+    turtle = turtle.Turtle()
+    turtle.pencolor("white")
+    turtle.penup()
+    turtle.setpos(0,-290)
+    turtle.write("Press 1, 2, 3, 4, or 5 to select your hand sign. \n           Press space to start the round", move=False, align="center", font=("Arial", 20, "normal"))
+    turtle.hideturtle()
+
 
 class Weapons:
     player_images = ["gifs/rock.gif", "gifs/paper.gif", "gifs/scissors.gif", "gifs/lizard.gif", "gifs/spock.gif"]
@@ -34,7 +43,7 @@ class Player:
     choosing = True
     wn = turtle.Screen()  # create the window
     weapon = Weapons.weapon[0]
-    wins = 0  # stats
+    score = 0
     history = []  # history of moves the player has made
     win_loss_history = []
 
@@ -53,11 +62,12 @@ class Player:
         self.turtle.shape("gifs/rock.gif")
 
     def choose_hand_sign(self):
-        Wn.wn.onkeypress(self.h1, "1")
-        Wn.wn.onkeypress(self.h2, "2")
-        Wn.wn.onkeypress(self.h3, "3")
-        Wn.wn.onkeypress(self.h4, "4")
-        Wn.wn.onkeypress(self.h5, "5")
+        if self.choosing == True:
+            Wn.wn.onkeypress(self.h1, "1")
+            Wn.wn.onkeypress(self.h2, "2")
+            Wn.wn.onkeypress(self.h3, "3")
+            Wn.wn.onkeypress(self.h4, "4")
+            Wn.wn.onkeypress(self.h5, "5")
         Wn.wn.onkeypress(self.h6, "space")
         Wn.wn.listen()
 
@@ -65,22 +75,27 @@ class Player:
     def h1(self):
         self.weapon = Weapons.weapon[0]
         self.turtle.shape(Weapons.player_images[0])
+        Tutorial.turtle.clear()
 
     def h2(self):
         self.weapon = Weapons.weapon[1]
         self.turtle.shape(Weapons.player_images[1])
+        Tutorial.turtle.clear()
 
     def h3(self):
         self.weapon = Weapons.weapon[2]
         self.turtle.shape(Weapons.player_images[2])
+        Tutorial.turtle.clear()
 
     def h4(self):
         self.weapon = Weapons.weapon[3]
         self.turtle.shape(Weapons.player_images[3])
+        Tutorial.turtle.clear()
 
     def h5(self):
         self.weapon = Weapons.weapon[4]
         self.turtle.shape(Weapons.player_images[4])
+        Tutorial.turtle.clear()
 
     def h6(self): # this keypress enters your choice or sets the state to the next round
         if self.choosing == True: # is the choosing is true it lets you enter your choice
@@ -90,28 +105,54 @@ class Player:
             Computer.turtle.seth(270-50)
             self.turtle.fd(60)
             Computer.turtle.fd(30)
+
+            self.turtle.seth(270 + 50)
+            Computer.turtle.seth(270 - 50)
+            self.turtle.fd(-60)
+            Computer.turtle.fd(-30)
+
+            self.turtle.seth(270 + 50)
+            Computer.turtle.seth(270 - 50)
+            self.turtle.fd(60)
+            Computer.turtle.fd(30)
+
+            self.turtle.seth(270 + 50)
+            Computer.turtle.seth(270 - 50)
+            self.turtle.fd(-60)
+            Computer.turtle.fd(-30)
+
+            self.turtle.seth(270 + 50)
+            Computer.turtle.seth(270 - 50)
+            self.turtle.fd(60)
+            Computer.turtle.fd(30)
             # Choose a hand for the computer
             choose_hand(Computer)
-            print(Player.weapon + Computer.weapon)
+            print(Player.weapon +" vs "+ Computer.weapon)
             if get_winner(self.weapon, Computer.weapon) == "winner":
+                print(Player.history)
+                Player.score +=1
+                Score()
                 Scorekeeper.turtle.shape(Scorekeeper.shapes[1])
             elif get_winner(self.weapon, Computer.weapon) == "tie":
                 Scorekeeper.turtle.shape(Scorekeeper.shapes[2])
             else :
                 Scorekeeper.turtle.shape(Scorekeeper.shapes[3])
-        else:
+                Computer.score += 1
+                Score()
+        else: #reset the hands for the next round
             self.turtle.setpos(-100, -120)  # move player hand back to start position
             Computer.turtle.setpos(70, -100)  # move computer hand back to start position
             self.choosing = True
+            self.history.append(self.weapon) # add the choice to the Player.history list
             Scorekeeper.turtle.shape(Scorekeeper.shapes[0])
             Computer.turtle.shape(Weapons.computer_images[0])
-            self.turtle.shape(Weapons.player_images[0])
             self.weapon = Weapons.weapon[0]
-            self.history.append(self.weapon)
+            self.turtle.shape(Weapons.player_images[0])
+
             return
 
 class Computer:
-    wins = 0
+    score = 0
     weapon = Weapons.weapon[0]
     wn = Wn.wn  # create the window
     turtle = turtle.Turtle()
@@ -125,8 +166,15 @@ class Computer:
         self.turtle.resizemode("auto")
 
 def choose_hand(self):
-    print(Player.history[:])
+    """
+    choose a weapon and shape for an object
+    :param self: an object with variables shape and weapon
+    """
+
     w = random.randint(0, 4)
+    if len(Player.history) > 0:
+        if Player.history[-1] == "rock":
+            w = 1
     self.weapon = Weapons.weapon[w]
     self.turtle.shape(Weapons.computer_images[w])
 
@@ -147,9 +195,27 @@ class Scorekeeper:
             self.wn.addshape(self.shapes[i])
         self.turtle.shape(self.shape)
 
+class Score:
+    turtle = turtle.Turtle()
+    turtle.penup()
+    turtle.setpos(0, 110)
+    turtle.hideturtle()
+    def __init__(self):
+        """Refresh the score"""
+        self.turtle.clear()
+        #self.turtle.pencolor("white")
+        self.turtle.write(str(Player.score)+"                "+str(Computer.score), move=False, align="center",
+                 font=("Arial", 20, "normal"))
+
 
 
 def get_winner(self, computer):
+    """
+    return whether the player beat the computer tied or lost
+    :param self
+    :param computer
+    :return "winner", "loser" or "tie".
+    """
     user = self
     #Weapons.weapon[user]
     #Weapons.weapon[computer]
@@ -181,14 +247,14 @@ def get_winner(self, computer):
 
 
 def main():
-    global rounds_played
-    State()
+    Score()
     Computer()
     Scorekeeper()
     Player()
+    Tutorial()
     get_winner(Player.weapon,Computer.weapon)
     Wn.wn.mainloop()
-    rounds_played = 0
+
 
 
 
